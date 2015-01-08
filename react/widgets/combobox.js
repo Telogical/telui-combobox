16b14996f4b61d467b1477cc5e338ab950f1e21e
Jesse Harlin <harlinjesse@gmail.com>
2015-01-08 01:59:30 -0600

    stable, minus clear x

--- react/widgets/combobox.js
+++ react/widgets/combobox.js
@@ -24,12 +24,9 @@ function Combobox(ui) {
         down = eve.which === 40,
         enter = eve.which === 13,
         tab = eve.which === 9,
-        backspace = eve.which === 8,
-        deletekey = eve.which === 46,
         menu = this.refs.menu,
         list = menu.refs.list,
-        elMenu = menu.getDOMNode(),
-        elList = list.getDOMNode();
+        elMenu = menu.getDOMNode();
 
       function hoverItem(childRefs) {
         function byHovered(child) {
@@ -114,7 +111,6 @@ function Combobox(ui) {
           elLi.click();
           return;
         }
-
       }
 
       if (up) {
@@ -133,15 +129,29 @@ function Combobox(ui) {
       if (tab) {
         selectItem();
       }
+    },
 
-      if (backspace || deletekey) {
+    __openMenu: function openMenu(eve) {
 
-        this.setState({
-          value: null,
-          inputVal: eve.target.value || ''
-        });
+      var alphanum = eve.which > 47 && eve.which < 91,
+        upDown = eve.which === 40 || eve.which === 38,
+        backDel = eve.which === 8 || eve.which === 46,
+        extAlpha = eve.which > 185 && eve.which < 223,
+        legitKeystroke = (alphanum || upDown || backDel || extAlpha);
+
+      //be more explicit
+      if (!legitKeystroke) {
+        return;
       }
 
+      var model = this.props;
+
+      this.setState({
+        inputVal: ''
+      });
+
+      model.buttonScope.value = true;
+      model.buttonScope.$apply();
     },
     __closeMenu: function closeMenu(eve) {
       var inputNode = this.refs.input.getDOMNode(),
@@ -160,29 +170,21 @@ function Combobox(ui) {
       var model = this.props;
 
       this.setState({
-        value: null,
         inputVal: eve.target.value || ''
       });
+    },
 
-      model.scope.value = null;
-      model.scope.$apply();
+    __clear: function clear() {
 
-      console.log('__onInputChange', model.value, model);
     },
     __onInputFocus: function () {
-      var model = this.props;
-      model.buttonScope.value = true;
-      model.buttonScope.$apply();
+
     },
     __onInputBlur: function () {
-      var model = this.props;
-      model.buttonScope.value = false;
-      //model.buttonScope.$apply();
-      console.log('..blur');
+      this.props.buttonScope.value = false;
     },
     __onMenuChange: function onMenuChange(value) {
-      var model = this.props,
-        _value = value.value;
+      var model = this.props;
 
       this.setState({
         value: value,
@@ -190,20 +192,45 @@ function Combobox(ui) {
       });
 
       model.scope.$apply(function (scope) {
-        scope.value = _value;
+        scope.value = value.value;
       });
+
+      this.refs.input.getDOMNode().focus();
+    },
+
+    __centerMenuValue: function (value) {
+
+      if (!value) {
+        return;
+      }
+
+      function byValue(ref) {
+        if (ref.props.value && value === ref.props.value.value) {
+          return ref;
+        }
+      }
+
+      var menu = this.refs.menu,
+        list = menu.refs.list,
+        elMenu = menu.getDOMNode();
+
+      var li = (value.id) ?
+        list.refs[value.id] :
+        _.find(list.refs, byValue);
+
+      if (li) {
+        var elLi = li.getDOMNode(),
+          mRect = elMenu.getBoundingClientRect();
+        elMenu.scrollTop = elLi.offsetTop - (mRect.height * 0.5);
+      }
+
     },
     componentDidUpdate: function componentDidUpdate() {
-      var body = document.body,
-        model = this.props,
+      var model = this.props,
         input = this.refs.input.getDOMNode();
 
       if (this.refs.dropdown) {
-        var elDropdown = this.refs.dropdown.getDOMNode(),
-          menu = this.refs.menu,
-          elMenu = menu.getDOMNode(),
-          list = menu.refs.list,
-          docEl = document.documentElement;
+        var elDropdown = this.refs.dropdown.getDOMNode();
 
         //positioning
         elDropdown.style.width = this._toPx(input.offsetWidth);
@@ -212,32 +239,19 @@ function Combobox(ui) {
 
         //eventing
         input.addEventListener('keydown', this.__keystrokeNavigation);
+        input.removeEventListener('keydown', this.__openMenu);
         document.addEventListener('click', this.__closeMenu);
 
         input.focus();
-
-        //center on active item, if any
-        if (model.value) {
-          //handle non Id ones.
-
-          if (model.value.id) {
-            var li = list.refs[model.value.id];
-            var elLi = li.getDOMNode(),
-              mRect = elMenu.getBoundingClientRect();
-
-            elMenu.scrollTop = elLi.offsetTop - (mRect.height * 0.5);
-          } else {
-
-            console.log('primitives', list.refs);
-          }
-
-        }
+        this.__centerMenuValue(model.value);
 
         return;
       }
 
+      input.addEventListener('keydown', this.__openMenu);
       input.removeEventListener('keydown', this.__keystrokeNavigation);
       document.removeEventListener('click', this.__closeMenu);
+
       return;
     },
     render: function render() {
@@ -247,7 +261,8 @@ function Combobox(ui) {
         domx = React.DOM,
         model = this.props,
         row = 'w-12 w-alpha w-omega',
-        key = model.id;
+        key = model.id,
+        inputVal;
 
       //templating
       var labelProp = model.labelProp,
@@ -255,22 +270,14 @@ function Combobox(ui) {
         labelTemplateString = isTemplate ? labelProp : '<%= ' + labelProp + '%>',
         labelTemplate = _.template(labelTemplateString);
 
-
-
       function toComboDataModel(d) {
-        //TODO put template here.
-
         var cbModel = {
-          label: d,
+          label: _.isObject(d) ? labelTemplate(d) : d,
           value: d
         };
 
-        if (_.isObject(d)) {
-          cbModel.label = labelTemplate(d);
-
-          if (d.id) {
-            cbModel.id = d.id;
-          }
+        if (d.id) {
+          cbModel.id = d.id;
         }
 
         return cbModel;
@@ -280,24 +287,22 @@ function Combobox(ui) {
         var label = (_d.label || '').toLowerCase(),
           val = (inputVal || '').toLowerCase();
 
-        if (model.value) {
-          return _d;
-        }
-
         if (_.contains(label, val)) {
           return _d;
         }
       }
 
-      var inputVal = this.state.inputVal || '';
-
-      //sync models
-      var outOfSync = model.value && !(this.__equals(model.value, this.state.value));
+      var outOfSync = model.value && !(this.__equals(model.value, this.state.value.value));
 
       if (outOfSync) {
         this.state.value = toComboDataModel(model.value);
-        this.state.inputVal = this.state.value.label;
-        inputVal = this.state.inputVal;
+      }
+
+      if (model.buttonScope.value) {
+        inputVal = this.state.inputVal || '';
+      } else {
+        this.state.inputVal = '';
+        inputVal = this.state.value ? this.state.value.label : '';
       }
 
       //build component
@@ -338,10 +343,6 @@ function Combobox(ui) {
           onBlur: this.__onInputBlur
         };
 
-      if (model.value && model.buttonScope.value) {
-        inputAttrs.value = '';
-      }
-
       var btnModel = {
         appearance: 'button',
         iconPrimary: model.iconPrimary || 'carat-1-s',
@@ -358,25 +359,26 @@ function Combobox(ui) {
         input = domx.input(inputAttrs),
         buttonFrame = domx.div(buttonFrameAttrs, button),
         inputFrame = domx.div(inputFrameAttrs, input),
-        contentFrame = domx.div(contentFrameAttrs, [buttonFrame, inputFrame]);
+        inputRow = [buttonFrame, inputFrame];
+
+      if (model.value) {
+        //push in close button
+      }
+      var contentFrame = domx.div(contentFrameAttrs, inputRow);
 
       var contents = [
           labelFrame,
           contentFrame
       ];
 
-      //add menu if present
       if (model.buttonScope.value) {
 
-        //internal data list
         var _data = _
           .chain(model.data)
           .map(toComboDataModel)
           .filter(byInputText)
           .value();
 
-        //todo memoize this, or
-        //provide some option to
         var menuModel = {
           id: key + '_menu',
           data: _data || [],
@@ -404,43 +406,9 @@ function Combobox(ui) {
         contents.push(dropdown);
       }
 
-      var frame = domx.div(frameAttrs, contents);
-      return frame;
+      return domx.div(frameAttrs, contents);
     }
   });
 }
 
 module.exports = Combobox;
-
-// <div data-ng-id="combobox" class="waffles ui-widget ui-combobox ui-corner-all">
-//    <div class="w-12 w-alpha w-omega">
-//        <div class="w-12 w-alpha w-omega" for="{{id}}_input">
-//            <label class="ui-combobox-label" for="{{id}}_input">{{label}}</label>
-//        </div>
-//        <div class="w-12 w-alpha w-omega">
-//            <div class="ui-combobox-dropdownbutton-frame">
-//                <telui-button
-//                        id="{{id}}_dropdownbutton"
-//                        text="false"
-//                        icon-primary="ui-icon-carat-1-s"
-//                        class="ui-combobox-dropdownbutton"
-//                        click="dropdownButton()"
-//                        tabindex="-1"
-//                        >
-//                </telui-button>
-//            </div>
-//            <div class="ui-combobox-input-frame w-v-1">
-//                <div
-//                        id="{{id}}_placeholder_frame"
-//                        class="ui-shim-placeholder-frame"
-//                        data-ng-show="isEmpty"
-//                        data-ng-click="focusInput()"
-//                        data-role="placeholder"
-//                        >
-//                    <div id="{{id}}_placeholder" class="ui-shim-placeholder">{{placeholder}}</div>
-//                </div>
-//                <input id="{{id}}_input" class="ui-combobox-input"/>
-//            </div>
-//        </div>
-//    </div>
-//</div>
--- src/telui.combobox.old.js
+++ src/telui.combobox.old.js
@@ -8,249 +8,285 @@ require('TelUI-Core/lib/jquery-ui/jquery.ui.autocomplete.js');
 var TelogicalUi = angular.module('TelUI');
 
 TelogicalUi
-    .directive('teluiCombobox', ['$http', '$templateCache',
+  .directive('teluiCombobox', ['$http', '$templateCache',
         function ($http, $templateCache) {
-            'use strict';
+      'use strict';
 
-            function link($scope, $element) {
-                //TODO: switch _input and _button with $input and $button
+      function link($scope, $element) {
+        //TODO: switch _input and _button with $input and $button
 
-                var initialized = false,
-                    _input = $element.find('input:first'),
-                    _button = $element.find('.ui-button');
+        var initialized = false,
+          _input = $element.find('input:first'),
+          _button = $element.find('.ui-button');
 
-                function init() {
-                    _input = $element.find('input:first');
-                    _button = $element.find('.ui-button');
+        function init() {
+          _input = $element.find('input:first');
+          _button = $element.find('.ui-button');
 
-                    function select(eve, ui) {
-                        $scope.$apply(function () {
-                            eve.preventDefault();
-                            $scope.value = ui.item.value;
-                            $scope.staleValue = ui.item.label;
-                            _input.val(ui.item.label);
-                            $scope.isEmpty = !ui.item.label.length;
-                        });
-                    }
+          function select(eve, ui) {
+            $scope.$apply(function () {
+              eve.preventDefault();
+              $scope.value = ui.item.value;
+              $scope.staleValue = ui.item.label;
+              _input.val(ui.item.label);
+              $scope.isEmpty = !ui.item.label.length;
+            });
+          }
 
-                    function focus(eve, ui) {
-                        if (typeof ui.item === 'object') {
-                            $scope.$apply(function () {
-                                eve.preventDefault();
-                                _input.val(ui.item.label);
-                                $scope.isEmpty = !ui.item.label.length;
-                            });
-                        }
-                    }
+          function focus(eve, ui) {
+            if (typeof ui.item === 'object') {
+              $scope.$apply(function () {
+                eve.preventDefault();
+                _input.val(ui.item.label);
+                $scope.isEmpty = !ui.item.label.length;
+              });
+            }
+          }
 
-                    var autoCompleteOptions = {
-                        delay: 0,
-                        minLength: 0,
-                        source: prepareData($scope.data),
-                        select: select,
-                        focus: focus,
-                        open: open
-                    };
+          var autoCompleteOptions = {
+            delay: 0,
+            minLength: 0,
+            source: prepareData($scope.data),
+            select: select,
+            focus: focus,
+            open: open
+          };
 
-                    initialized =
-                        _input
-                        .autocomplete(autoCompleteOptions);
+          initialized =
+            _input
+            .autocomplete(autoCompleteOptions);
 
-                    _button
-                        .off('.combobox')
-                        .on('click.combobox', dropdownButton);
+          _button
+            .off('.combobox')
+            .on('click.combobox', dropdownButton);
 
-                    _input
-                        .off('.combobox')
-                        .on('keydown.combobox', keydown);
+          _input
+            .off('.combobox')
+            .on('keydown.combobox', keydown);
 
-                    $('body')
-                        .on('click.combobox', closeSelect);
-                }
+          $('body')
+            .on('click.combobox', closeSelect);
+        }
 
-                function prepareData(data) {
-                    var _data = $.extend(true, [], data);
+        function prepareData(data) {
+          var _data = $.extend(true, [], data);
 
-                    function assignObject(i) {
-                        var obj = {
-                            label: _data[i][$scope.labelProp],
-                            value: _data[i]
-                        };
-                        return obj;
-                    }
+          function assignObject(i) {
+            var obj = {
+              label: _data[i][$scope.labelProp],
+              value: _data[i]
+            };
+            return obj;
+          }
 
-                    for (var i = 0, ii = _data.length; i < ii; i++) {
-                        if (!_data[i].label && !_data[i].value) {
-                            _data[i] = assignObject(i);
-                        }
-                    }
-                    return _data;
-                }
+          for (var i = 0, ii = _data.length; i < ii; i++) {
+            if (!_data[i].label && !_data[i].value) {
+              _data[i] = assignObject(i);
+            }
+          }
+          return _data;
+        }
 
-                function updateData(data) {
-                    var autocompleteOpts = {
-                        source: prepareData(data),
-                        open: open
-                    };
+        function updateData(data) {
+          var autocompleteOpts = {
+            source: prepareData(data),
+            open: open
+          };
 
-                    //triggering search forces list to update if new payload
-                    _input
-                        .autocomplete(autocompleteOpts);
-                }
+          //triggering search forces list to update if new payload
+          _input
+            .autocomplete(autocompleteOpts);
+        }
 
-                function open(eve, ui) {
-                    var widget = $(this).data('ui-autocomplete'),
-                        menu = widget.menu,
-                        $ul = menu.element,
-                        id = $ul.attr('id');
+        function open(eve, ui) {
+          var widget = $(this).data('ui-autocomplete'),
+            menu = widget.menu,
+            $ul = menu.element,
+            id = $ul.attr('id');
 
-                    $ul
-                        .css({
-                            'max-height': $scope.maxHeight || '100%',
-                            'overflow': 'auto'
-                        });
+          $ul
+            .css({
+              'max-height': $scope.maxHeight || '100%',
+              'overflow': 'auto'
+            });
 
-                    setTimeout(function () {
-                        $(window).resize();
-                    });
-                }
+          setTimeout(function () {
+            $(window).resize();
+          });
+        }
 
-                function updateValue(val) {
-                    $scope.value = val;
-                    if (val && typeof val === 'object') {
-                        _input.val(val[$scope.labelProp]);
-                    } else {
-                        _input.val(val);
-                    }
-                    $scope.isEmpty = !_input.val().length;
-                }
+        function updateValue(val) {
+          $scope.value = val;
+          if (val && typeof val === 'object') {
+            _input.val(val[$scope.labelProp]);
+          } else {
+            _input.val(val);
+          }
+          $scope.isEmpty = !_input.val().length;
+        }
 
-                function updateEnablement(value) {
-                    value =
-                        (value == true || value == 'true') ?
-                        true :
-                        false;
+        function updateEnablement(value) {
+          value =
+            (value == true || value == 'true') ?
+            true :
+            false;
 
-                    _input = $element.find('input:first');
-                    _button = $element.find('.ui-button');
+          _input = $element.find('input:first');
+          _button = $element.find('.ui-button');
 
-                    _input.prop('disabled', value);
-                    _button.prop('disabled', value);
-                    if (value) {
-                        _input.autocomplete('disable');
-                        _button.button('disable');
-                    } else {
-                        _input.autocomplete('enable');
-                        _button.button('enable');
-                    }
-                }
+          _input.prop('disabled', value);
+          _button.prop('disabled', value);
+          if (value) {
+            _input.autocomplete('disable');
+            _button.button('disable');
+          } else {
+            _input.autocomplete('enable');
+            _button.button('enable');
+          }
+        }
 
-                function focusInput() {
-                    _input.focus();
-                }
+        function focusInput() {
+          _input.focus();
+        }
 
-                function closeSelect(eve, target) {
+        function closeSelect(eve, target) {
 
-                    _input = $element.find('input:first');
-                    _button = $element.find('.ui-button');
-                    if (_input.hasClass('ui-autocomplete-input')) {
-                        _input
-                            .autocomplete('search', '')
-                            .autocomplete('close');
-                    }
-                    _button.blur();
+          _input = $element.find('input:first');
+          _button = $element.find('.ui-button');
+          if (_input.hasClass('ui-autocomplete-input')) {
+            _input
+              .autocomplete('search', '')
+              .autocomplete('close');
+          }
+          _button.blur();
 
-                    if ($scope.staleValue) {
-                        _input.val($scope.staleValue);
-                        $scope.staleValue = '';
+          if ($scope.staleValue) {
+            _input.val($scope.staleValue);
+            $scope.staleValue = '';
 
-                    }
+          }
 
-                    $scope.$apply(function () {
-                        $scope.isEmpty = !_input.val().length;
-                    });
-                }
+          $scope.$apply(function () {
+            $scope.isEmpty = !_input.val().length;
+          });
+        }
 
-                function dropdownButton() {
-                    function clickEvent() {
-                        _input = $element.find('input:first');
-                        _button = $element.find('.ui-button');
-                        if (_input.autocomplete('widget').is(':visible')) {
-                            closeSelect();
-                            return;
-                        }
-
-                        $scope.staleValue = _input.val();
-
-                        _input
-                            .val('')
-                            .keydown()
-                            .autocomplete('search', '')
-                            .autocomplete('widget');
-
-                        _button
-                            .focus();
-
-                        $scope.$apply(function () {
-                            $scope.isEmpty = !_input.val().length;
-                        });
-
-                    }
-
-                    if ($scope.debounceClick) {
-                        clearTimeout($scope.debounceClick);
-                    }
-                    $scope.debounceClick = setTimeout(clickEvent);
-                }
-
-                function keydown(eve) {
-                    if (eve.which === 13 && _input.autocomplete('widget').is(':visible')) {
-                        _input
-                            .autocomplete('widget')
-                            .find('.ui-menu-item:first > a')
-                            .click();
-
-                        _input.autocomplete('close');
-                        _button.blur();
-                    }
-
-                    setTimeout(function evaluateIsEmpty() {
-                        $scope.isEmpty = !_input.val().length;
-                        $scope.$apply();
-                    });
-                }
-
-                $scope.data = $scope.data || [];
-                $scope.value = $scope.value || '';
-                $scope.placeholder = $scope.placeholder || '';
-                $scope.dropdownButton = dropdownButton;
-                $scope.focusInput = focusInput;
-                $scope.isEmpty = true;
-
-                $scope.$watch('data', updateData, true);
-                $scope.$watch('value', updateValue, true);
-                $scope.$watch('ngDisabled', updateEnablement, true);
-
-                setTimeout(init);
+        function dropdownButton() {
+          function clickEvent() {
+            _input = $element.find('input:first');
+            _button = $element.find('.ui-button');
+            if (_input.autocomplete('widget').is(':visible')) {
+              closeSelect();
+              return;
             }
 
-            return {
-                restrict: 'E',
-                require: '',
-                template: require('../ui/partials/telui-combobox-partial.html'),
-                replace: true,
-                transclude: false,
-                scope: {
-                    'id': '@',
-                    'data': '=?',
-                    'ngDisabled': '=?',
-                    'value': '=?',
-                    'label': '@',
-                    'labelProp': '@',
-                    'maxHeight': '@',
-                    'placeholder': '@'
-                },
-                link: link
-            };
+            $scope.staleValue = _input.val();
+
+            _input
+              .val('')
+              .keydown()
+              .autocomplete('search', '')
+              .autocomplete('widget');
+
+            _button
+              .focus();
+
+            $scope.$apply(function () {
+              $scope.isEmpty = !_input.val().length;
+            });
+
+          }
+
+          if ($scope.debounceClick) {
+            clearTimeout($scope.debounceClick);
+          }
+          $scope.debounceClick = setTimeout(clickEvent);
+        }
+
+        function keydown(eve) {
+          if (eve.which === 13 && _input.autocomplete('widget').is(':visible')) {
+            _input
+              .autocomplete('widget')
+              .find('.ui-menu-item:first > a')
+              .click();
+
+            _input.autocomplete('close');
+            _button.blur();
+          }
+
+          setTimeout(function evaluateIsEmpty() {
+            $scope.isEmpty = !_input.val().length;
+            $scope.$apply();
+          });
+        }
+
+        $scope.data = $scope.data || [];
+        $scope.value = $scope.value || '';
+        $scope.placeholder = $scope.placeholder || '';
+        $scope.dropdownButton = dropdownButton;
+        $scope.focusInput = focusInput;
+        $scope.isEmpty = true;
+
+        $scope.$watch('data', updateData, true);
+        $scope.$watch('value', updateValue, true);
+        $scope.$watch('ngDisabled', updateEnablement, true);
+
+        setTimeout(init);
+      }
+
+      return {
+        restrict: 'E',
+        require: '',
+        template: require('../ui/partials/telui-combobox-partial.html'),
+        replace: true,
+        transclude: false,
+        scope: {
+          'id': '@',
+          'data': '=?',
+          'ngDisabled': '=?',
+          'value': '=?',
+          'label': '@',
+          'labelProp': '@',
+          'maxHeight': '@',
+          'placeholder': '@'
+        },
+        link: link
+      };
         }
     ]);
+
+
+
+
+// <div data-ng-id="combobox" class="waffles ui-widget ui-combobox ui-corner-all">
+//    <div class="w-12 w-alpha w-omega">
+//        <div class="w-12 w-alpha w-omega" for="{{id}}_input">
+//            <label class="ui-combobox-label" for="{{id}}_input">{{label}}</label>
+//        </div>
+//        <div class="w-12 w-alpha w-omega">
+//            <div class="ui-combobox-dropdownbutton-frame">
+//                <telui-button
+//                        id="{{id}}_dropdownbutton"
+//                        text="false"
+//                        icon-primary="ui-icon-carat-1-s"
+//                        class="ui-combobox-dropdownbutton"
+//                        click="dropdownButton()"
+//                        tabindex="-1"
+//                        >
+//                </telui-button>
+//            </div>
+//            <div class="ui-combobox-input-frame w-v-1">
+//                <div
+//                        id="{{id}}_placeholder_frame"
+//                        class="ui-shim-placeholder-frame"
+//                        data-ng-show="isEmpty"
+//                        data-ng-click="focusInput()"
+//                        data-role="placeholder"
+//                        >
+//                    <div id="{{id}}_placeholder" class="ui-shim-placeholder">{{placeholder}}</div>
+//                </div>
+//                <input id="{{id}}_input" class="ui-combobox-input"/>
+//            </div>
+//        </div>
+//    </div>
+//</div>
